64550bb6c9d772c87161137a6922372f67bf0805
Sammy Krosoft <33433229+SammyKrosoft@users.noreply.github.com>
2021-02-10 23:54:41 -0500

    Update GetPerfCounterStatsFromBLG.ps1

--- GetPerfCounterStatsFromBLG.ps1
+++ GetPerfCounterStatsFromBLG.ps1
@@ -1,33 +1,76 @@
+[CmdletBinding()]
+Param([string]$BLGFileName="ExchangeDiagnosticsDailyPerformanceLog_02071407.blg",
+      [string]$CounterFilter1,
+      [string]$CounterFilter2="I/O Database Reads (Attached) Average Latency",
+      [string]$CounterFilter3
+      )
 
+$OutputFile = "$($env:USERPROFILE)\Documents\CountersSummary_$(Get-Date -Format yyyMMdd_hhmmss).csv"
+
+# Defining and starting chronograph
+Write-host "Starting..." -ForegroundColor Green
 $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
 
+Write-Host "Current Parameters:" -ForegroundColor Green
+Write-host "BLG File          :     $BLGFileName" -ForegroundColor Yellow
+Write-Host "Counter Filter 1  :     $CounterFilter1" -ForegroundColor Yellow
+Write-Host "Counter Filter 2  :     $CounterFilter2" -ForegroundColor Yellow
+Write-Host "Counter Filter 3  :     $CounterFilter3" -ForegroundColor Yellow
+
+# If there is nothing on the CounterFilter parameters, we consider a wildcard, otherwise, we add wildcards to find counters with substrings <3
+If ([String]::IsNullOrEmpty($BLGFileName)){$BLGFileName = "*.blg"}
+If ([string]::IsNullOrEmpty($CounterFilter1)){$CounterFilter1 = "*"}else{$CounterFilter1 = "*" + $CounterFilter1 + "*"}
+If ([string]::IsNullOrEmpty($CounterFilter2)){$CounterFilter2 = "*"}else{$CounterFilter2 = "*" + $CounterFilter2 + "*"}
+If ([string]::IsNullOrEmpty($CounterFilter3)){$CounterFilter3 = "*"}else{$CounterFilter3 = "*" + $CounterFilter3 + "*"}
+
+Write-Host "Processed filters:"
+Write-Host "Counter Filter 1  :     $CounterFilter1" -ForegroundColor Magenta
+Write-Host "Counter Filter 2  :     $CounterFilter2" -ForegroundColor Magenta
+Write-Host "Counter Filter 3  :     $CounterFilter3" -ForegroundColor Magenta
+
 #Loading all counters present in the target BLG:
-#$counterList = @(Import-Counter -path $env:exchangeinstallpath\Logging\Diagnostics\DailyPerformanceLogs\ExchangeDiagnosticsDailyPerformanceLog_02071407.blg -ListSet * | ForEach-Object {$_.Paths})
-$counterList = @(Import-Counter -path $env:exchangeinstallpath\Logging\Diagnostics\DailyPerformanceLogs\ExchangeDiagnosticsDailyPerformanceLog_02071407.blg -ListSet * | Foreach-Object {If ($_.CounterSetType -eq "SingleInstance"){$_.Paths} Else {$_.PathsWithInstances}})
+Write-Host "Getting counters list from BLG file, please wait..." -ForegroundColor DarkRed
+$counterList = @(Import-Counter -path "$($env:exchangeinstallpath)\Logging\Diagnostics\DailyPerformanceLogs\$BLGFileName" -ListSet * | Foreach-Object {If ($_.CounterSetType -eq "SingleInstance"){$_.Paths} Else {$_.PathsWithInstances}})
 
-# To load all counters list from all BLGs in the DailyPerformanceLogs Exchange install folders:
-#$counterList = @(Import-Counter -ListSet $env:exchangeinstallpath\Logging\Diagnostics\DailyPerformanceLogs\*.blg)
-$Counter = "*I/O Database Reads (Attached) Average Latency*"
-$CounterInstance = "*DAG1-DB*"
-#$Server = "E2016-02"
+$Tick = $StopWatch.Elapsed.totalseconds
+Write-host "Took $Tick seconds to load all counters from $BLGFileName"
 
+Write-Host "All counters loaded $($CounterList.Count) counters, now filtering ..." -ForegroundColor DarkRed
 #$counterListFiltered = $counterList -like "\\$Server\$Counter" 
-$counterListFiltered = $counterList -like "$Counter" 
-$counterListFiltered = $counterListFiltered -like "$CounterInstance"
-$counterListFiltered
+$counterListFiltered = $counterList -like "$CounterFilter1"
+$counterListFiltered = $counterListFiltered -like "$CounterFilter2"
+$CounterListFiltered = $counterListFiltered -like "$CounterFilter3"
 
+If ($counterListFiltered -eq 0){
+    Write-Host "no counters found with the filters provided. Exiting...";
+    $StopWatch.Stop()
+    $StopWatch.Elapsed.totalseconds | Out-Host
+    exit
+}
+Else {
+    write-Host "Counters filtered, we have now $($counterListFiltered.count) counters:" -ForeGroundColor yellow -BackgroundColor Blue
+    Foreach ($Item in $counterListFiltered){Write-Host $Item -Foregroundcolor yellow -BackgroundColor Blue}
+}
 
 #To import counters from all the BLGs in the DailyPerformanceLogs:
-#$Data = Import-Counter -Path $env:exchangeinstallpath\Logging\Diagnostics\DailyPerformanceLogs\*.blg -Counter $counterListFiltered
 #To import counters from only one BLG file in the DailyPerformanceLogs:
-$Data = Import-Counter -Path $env:exchangeinstallpath\Logging\Diagnostics\DailyPerformanceLogs\ExchangeDiagnosticsDailyPerformanceLog_02071407.blg -Counter $CounterListFiltered
+$Data = Import-Counter -Path "$($env:exchangeinstallpath)\Logging\Diagnostics\DailyPerformanceLogs\$BLGFileName" -Counter $CounterListFiltered
 
+$AllResults = @()
+$Data | Select -ExpandProperty CounterSamples | Group-Object Path | Foreach {   $Stats = $_ | Select -ExpandProperty Group | Measure-Object -Average -Minimum -Maximum CookedValue
+                                                                                $AllResults += [PSCustomObject]@{
+                                                                                                                    
+                                                                                                                    Counter=$_.Name
+                                                                                                                    Average=$Stats.Average
+                                                                                                                    Minimum=$Stats.Minimum
+                                                                                                                    Maximum=$Stats.Maximum
+                                                                                                                    Samples=$Stats.Count
+                                                                                                                    }                                                                                        
+                                                                            }
 $StopWatch.Stop()
-$StopWatch.Elapsed.totalseconds | Out-Host
+$FinalTime = $StopWatch.Elapsed.totalseconds
 
+Write-Host "Took $FinalTime seconds in total to run the script"
 
-$Data | fl *
-$Data | Select -ExpandProperty CounterSamples | Group-Object Path | Foreach {$_.Name;$_ | Select -ExpandProperty Group | Measure-Object -average -min -max CookedValue}
-
-
-
+$AllResults | Export-CSV -NoTypeInformation -Path $OutputFile
+notepad $OutputFile
