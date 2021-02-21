5e74750e54cc44cec713e9975b893fcdabb90624
Sammy Krosoft <sammy@hotmail.fr>
2021-02-20 21:48:34 -0500

    Big update

--- Collect-SummarizedPerfmonCounterStats.ps1
+++ Collect-SummarizedPerfmonCounterStats.ps1
@@ -19,94 +19,126 @@ Average Latency" counters for all the databases with "DB" in their names.
 #>
 
 [CmdletBinding()]
-
-
 Param([string]$BLGFileName,
-      [string]$CounterFilter1,
-      [string]$CounterFilter2,
-      [string]$CounterFilter3,
+      [string[]]$ArrayFilter,
+      [string]$CountersFile, #Note: this is for future use to enable passing list of counter path
       [string]$BLGFolder = "$($env:exchangeinstallpath)Logging\Diagnostics\DailyPerformanceLogs\"
       )
 
 #Setting up global variables that we'll reuse
+#Output file to local user launching the script's "My Documents" folder
 $OutputFile = "$($env:USERPROFILE)\Documents\CountersSummary_$(Get-Date -Format yyyMMdd_hhmmss).csv"
 
-# Defining and starting chronograph
+# Defining and starting stopwatch to measure script execution time
 Write-host "Starting..." -ForegroundColor Green
 $StopWatch = [System.Diagnostics.Stopwatch]::StartNew()
 
+If (!(Test-Path $BLGFolder)){
+      Write-Host "Folder $BLGFolder doesn't exist ... please run the script from an Exchange server which DailyPerformanceLogs folder is located in the Exchange Install path\Logging\Diagnostics folder, or specify an existing folder with .BLG files on it" -ForegroundColor Red
+      Exit
+}
 
-Write-Host "Current Parameters:" -ForegroundColor Green
-Write-host "BLG File          :     $BLGFileName" -ForegroundColor Yellow
-Write-Host "BLG folder path   :     $BLGFolder" -ForegroundColor Yellow
-Write-Host "Counter Filter 1  :     $CounterFilter1" -ForegroundColor Yellow
-Write-Host "Counter Filter 2  :     $CounterFilter2" -ForegroundColor Yellow
-Write-Host "Counter Filter 3  :     $CounterFilter3" -ForegroundColor Yellow
-
-# If there is nothing on the CounterFilter parameters, we consider a wildcard, otherwise, we add wildcards to find counters with substrings <3
 If ([String]::IsNullOrEmpty($BLGFileName)){
-    $BLGFileName = (Get-ChildItem -Path "$BLGFolder\*.blg" | Sort-Object LastWriteTime | Select -First 1).Name
-    }
-If ([string]::IsNullOrEmpty($CounterFilter1)){$CounterFilter1 = "*"}else{$CounterFilter1 = "*" + $CounterFilter1 + "*"}
-If ([string]::IsNullOrEmpty($CounterFilter2)){$CounterFilter2 = "*"}else{$CounterFilter2 = "*" + $CounterFilter2 + "*"}
-If ([string]::IsNullOrEmpty($CounterFilter3)){$CounterFilter3 = "*"}else{$CounterFilter3 = "*" + $CounterFilter3 + "*"}
+      $BLGFileName = (Get-ChildItem -Path "$BLGFolder\*.blg" | Sort-Object LastWriteTime | Select -First 1).Name
+      }
 
-Write-Host "Processed filters:" -ForegroundColor Green
-Write-Host "BLG file (if nothing specified, takes the oldest):" -ForegroundColor Magenta
-Write-host "                        $BLGFileName"
-Write-Host "BLG Folder Path   :     $BLGFolder" -ForegroundColor Magenta
-Write-Host "Counter Filter 1  :     $CounterFilter1" -ForegroundColor Magenta
-Write-Host "Counter Filter 2  :     $CounterFilter2" -ForegroundColor Magenta
-Write-Host "Counter Filter 3  :     $CounterFilter3" -ForegroundColor Magenta
+      Write-Host "Current Parameters:" -ForegroundColor Green
+      Write-host "BLG File          :     $BLGFileName" -ForegroundColor Yellow
+      Write-Host "BLG folder path   :     $BLGFolder" -ForegroundColor Yellow
 
+#region BLG file check
 #Verifying if BLG file(s) exist - doesn't apply if no BLG specified, as took the oldest one
 $ExchangeBLGDiagnosticsFilesPath = "$BLGFolder\$BLGFileName"
-If (!(Test-Path $ExchangeBLGDiagnosticsFilesPath)){Write-Host "File(s) $ExchangeBLGDiagnosticsFilesPath not found. Please specify valid BLG file" -ForegroundColor Red;exit;$StopWatch.Stop();$StopWatch.Elapsed.totalseconds | Out-Host}
+If (!(Test-Path $ExchangeBLGDiagnosticsFilesPath)){
+      Write-Host "File(s) $ExchangeBLGDiagnosticsFilesPath not found. Please specify valid BLG file" -ForegroundColor Red;
+      $StopWatch.Stop();
+      $StopWatch.Elapsed.totalseconds | Out-Host
+      exit;
+}
 
+#endregion BLG file check
+
+If ($CountersFile){
+      #Validate the file exists
+      if (!(Test-Path($CountersFile))){
+            Write-Host "The file $CountersFile doesn't exist ! Specify a valid file or use -CounterFilterX to search for counters..." -ForegroundColor Red -BackgroundColor Blue
+            $StopWatch.Stop();
+            $StopWatch.Elapsed.totalseconds | Out-Host
+            Exit
+      } Else {
+            Write-Host "File $CountersFile found, loading counters from file..." -ForegroundColor Green
+            $CountersListFromFile = Get-Content $CountersFile
+      }
+} Else {
+      If ([string]::IsNullOrEmpty($ArrayFilter)){
+            $ArrayFilter = "*"
+            Write-Host "No CountersFile and no ArrayFilter specified... using ArrayFilter = $ArrayFilter"
+      } Else {
+            $NbFilterStrings = $ArrayFilter.Count
+            for ($i=0;$i -lt $NbFilterStrings;$i++) {
+                  Write-Host "Treating Index of ArrayFilter #$i aka $($ArrayFilter[$i])"
+                  $ArrayFilter[$i]="*" + $ArrayFilter[$i] + "*"
+            }
+      }
+}
 
 #Loading all counters present in the target BLG:
-Write-Host "Getting counters list from BLG file, please wait..." -ForegroundColor DarkRed
-$counterList = @(Import-Counter -path "$ExchangeBLGDiagnosticsFilesPath" -ListSet * | Foreach-Object {If ($_.CounterSetType -eq "SingleInstance"){$_.Paths} Else {$_.PathsWithInstances}})
+Write-Host "Loading counters list from BLG file, please wait..." -ForegroundColor DarkRed
+#$CounterList = @(Import-Counter -path "$ExchangeBLGDiagnosticsFilesPath" -ListSet * | Foreach-Object {If ($_.CounterSetType -eq "SingleInstance"){$_.Paths} Else {$_.PathsWithInstances}})
+$CounterList = @(Import-Counter -path "$ExchangeBLGDiagnosticsFilesPath" -ListSet * | Foreach-Object {If ($_.CounterSetType -eq "SingleInstance"){$_ | Select -ExpandProperty Paths} Else {$_ | Select -ExpandProperty PathsWithInstances}})
+
+#exit
 
 $Tick = $StopWatch.Elapsed.totalseconds
 Write-host "Took $Tick seconds to load all counters from $BLGFileName"
+Write-Host "Found $($CounterList.count) counter(s) !" -ForegroundColor DarkRed -BackgroundColor DarkBlue
+Write-Host "Counters path loaded ! Filtering these as per ArrayFilter or Counters from file..." -ForegroundColor DarkRed
 
-Write-Host "All counters loaded $($CounterList.Count) counters, now filtering ..." -ForegroundColor DarkRed
-#$counterListFiltered = $counterList -like "\\$Server\$Counter" 
-$counterListFiltered = $counterList -like "$CounterFilter1"
-$counterListFiltered = $counterListFiltered -like "$CounterFilter2"
-$CounterListFiltered = $counterListFiltered -like "$CounterFilter3"
+$CounterListFiltered = $CounterList
 
-If ($counterListFiltered -eq 0){
-    Write-Host "no counters found with the filters provided. Exiting...";
-    $StopWatch.Stop()
-    $StopWatch.Elapsed.totalseconds | Out-Host
-    exit
+If ($CountersFile){
+      $CounterListFiltered = $CountersListFromFile
+}ElseIf (!($ArrayFilter -eq "*")){
+      Foreach ($Item in $ArrayFilter){
+            Write-Host "Filtering with $Item" -ForegroundColor Yellow
+            $CounterListFiltered = $CounterListFiltered | ? {$_ -like $Item}
+      }
+}Else{
+      Write-Host "-ArrayFilter is unspecified, using ArrayFilter = $ArrayFilter"
+      $CounterListFiltered = $CounterList
 }
-Else {
-    write-Host "Counters filtered, we have now $($counterListFiltered.count) counters:" -ForeGroundColor yellow -BackgroundColor Blue
-    Foreach ($Item in $counterListFiltered){Write-Host $Item -Foregroundcolor yellow -BackgroundColor Blue}
+
+$NumberOfCountersFromBLG = $CounterListFiltered.count
+
+If ($NumberOfCountersFromBLG -eq 0){
+      Write-Host "No counters found with the chosen filter or counter files"
 }
 
-#To import counters from all the BLGs in the DailyPerformanceLogs:
-#To import counters from only one BLG file in the DailyPerformanceLogs:
+Write-Host "List of counters filtered:"
+Foreach ($item in $CounterListFiltered){
+      write-host $Item -ForegroundColor Green
+}
+
+#Exit
+
+#import counters this time not only the paths, but all the values as well, for filtered counters 
 $Data = Import-Counter -Path "$ExchangeBLGDiagnosticsFilesPath" -Counter $CounterListFiltered -ErrorAction SilentlyContinue
 
 $AllResults = @()
 $Data | Select -ExpandProperty CounterSamples | Group-Object Path | Foreach {   $Stats = $_ | Select -ExpandProperty Group | Measure-Object -Average -Minimum -Maximum CookedValue
                                                                                 $AllResults += [PSCustomObject]@{
-                                                                                                                    
-                                                                                                                    Counter=$_.Name
-                                                                                                                    Minimum=$Stats.Minimum
-                                                                                                                    Average=$Stats.Average
-                                                                                                                    Maximum=$Stats.Maximum
-                                                                                                                    Samples=$Stats.Count
-                                                                                                                    }                                                                                        
+                                                                                Counter=$_.Name
+                                                                                Instance = $_.Group.InstanceName[0]
+                                                                                Minimum=$Stats.Minimum
+                                                                                Average=$Stats.Average
+                                                                                Maximum=$Stats.Maximum
+                                                                                Samples=$Stats.Count
+                                                                                                                  }                                                                                        
                                                                             }
 $StopWatch.Stop()
 $FinalTime = $StopWatch.Elapsed.totalseconds
 
 Write-Host "Took $FinalTime seconds in total to run the script"
 
-$AllResults | Select Counter, Samples, Minimum, Average, Maximum | Export-CSV -NoTypeInformation -Path $OutputFile
+$AllResults | Select Counter, Samples, Instance, Minimum, Average, Maximum | Export-CSV -NoTypeInformation -Path $OutputFile
 notepad $OutputFile
